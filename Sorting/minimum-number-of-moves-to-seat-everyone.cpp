f7b8f95fd639723a87b4c85eceb5e7c8ea49821c
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2024-06-12 20:52:35 -0600

    added "minimum number of moves to seat everyone"

---
+++ Sorting/minimum-number-of-moves-to-seat-everyone.cpp
@@ -0,0 +1,30 @@
+/* 
+    Approaches
+     1. Calculate the sum of the distances between each student and the closest seat
+    
+    Data Structures
+    Algorithms
+
+    Time Complexity
+     - O(n•log(n))
+    Space Complexity
+     - O(log(n))
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+        
+        int ans = 0;
+        for (int i = 0; i < seats.size(); i++) {
+            ans += abs(seats[i] - students[i]);
+        }
+
+        return ans;
+    }
+};
