5482700a9d00e4755ec771e415e3dfbd0815fad7
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2024-06-14 18:26:40 -0600

    added "minimum increment to make array unique"

---
+++ Sorting/minimum-increment-to-make-array-unique.cpp
@@ -0,0 +1,32 @@
+/* 
+    Approaches
+     1. Calculate the total increments needed so that each element is greater than the previous one in the sorted array
+
+    Data Structures
+    Algorithms
+
+    Time Complexity
+     - O(n•log(n))
+    Space Complexity
+     - O(log(n))
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        int ans = 0;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] <= nums[i - 1]) {
+                ans += nums[i - 1] - nums[i] + 1;
+                nums[i] = nums[i - 1] + 1;
+            }
+        }
+        
+        return ans;
+    }   
+};
