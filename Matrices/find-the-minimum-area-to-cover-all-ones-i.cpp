b58fdab433725ee104b48db21f9d0f849608f526
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2024-06-26 19:19:30 -0600

    added "find the minimum area to cover all ones i"

---
+++ Matrices/find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -0,0 +1,37 @@
+/* 
+    Approaches
+     1. Locate the farthest element with a 1 to the left, right, bottom, and top, and then calculate the area
+
+    Data Structures
+    Algorithms
+
+    Time Complexity
+     - O(m•n)
+    Space Complexity
+     - O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minimumArea(vector<vector<int>>& grid) {
+        int l = INT_MAX, r = 0, b = INT_MAX, t = 0;
+        
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 1) {
+                    l = min(l, j);
+                    r = max(r, j);
+                    b = min(b, i);
+                    t = max(t, i);
+                }
+            }
+        }
+        return (r - l + 1) * (t - b + 1);
+    }
+};
+
+
+
