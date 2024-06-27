ba79cbb6a24ac7fbbcd3a1f049b33bfa99164e33
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2024-06-26 18:46:22 -0600

    added "minimum average of smallest and largest elements"

---
+++ Two Pointers/minimum-average-of-smallest-and-largest-elements.cpp
@@ -0,0 +1,31 @@
+/* 
+    Approaches
+     1. Iterate from both ends of the sorted array, calculate the average, and update the minimum average
+
+    Data Structures
+    Algorithms
+
+    Time Complexity
+     - O(n•log(n))
+    Space Complexity
+     - O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double minimumAverage(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        double ans = INT_MAX;
+        int l = 0;
+        int r = nums.size() - 1;
+        while (l < r) {
+            ans = min(ans, static_cast<double>((nums[r] + nums[l]))/2);
+            l++;
+            r--;
+        }
+        return ans;
+    }
+};
