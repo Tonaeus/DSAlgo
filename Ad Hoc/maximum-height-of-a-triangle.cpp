9219e1f045a777e4a14da5a3c44ea0a7b6ed7821
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2024-07-01 11:51:37 -0600

    added "maximum height of a triangle"

---
+++ Ad Hoc/maximum-height-of-a-triangle.cpp
@@ -0,0 +1,40 @@
+/* 
+    Approaches
+     1. Alternative between the two colours to build each row of the triangle
+      a. Consider that the triangle can start with either a red or blue ball
+
+    Data Structures
+    Algorithms
+
+    Time Complexity
+     - O(sqrt(n)) -> n = total number of balls
+    Space Complexity
+     - O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxHeightOfTriangle(int red, int blue) {
+        return max(helper(red, blue), helper(blue, red));
+    }
+private: 
+    int helper(int odd, int even) {
+        int ans = 0;
+        int count = 1;
+        while (1) {
+            if (count % 2 != 0) {
+                if ((odd -= count) >= 0) ans++;
+                else break;
+            }
+            else {
+                if ((even -= count) >= 0) ans++;
+                else break;
+            }
+            count++;
+        }
+        return ans;
+    }
+};
