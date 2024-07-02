41fbac90aa0cc5e7f06e22c84fef1c29ab008d48
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2024-07-01 20:07:56 -0600

    added "valid anagram"

---
+++ Arrays/valid-anagram.cpp
@@ -0,0 +1,31 @@
+/* 
+    Approaches
+     1. Count the frequency of each character in both strings and check if they are the same
+
+    Data Structures
+     - Array
+    Algorithms
+
+    Time Complexity
+     - O(max(n, m))
+    Space Complexity
+     - O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> sFreq(26);
+        vector<int> tFreq(26);
+
+        for (int i = 0; i < s.length() && i < t.length(); i++) {
+            sFreq[s[i]-'a']++;
+            tFreq[t[i]-'a']++;
+        }
+
+        return s.length() == t.length() && sFreq == tFreq;
+    }
+};
