bd0465fdd2480f4bc14c43723d765a9a08d4fb59
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2024-07-01 23:30:51 -0600

    added "group anagrams"

---
+++ Hash Maps/group-anagrams.cpp
@@ -0,0 +1,69 @@
+/* 
+    Approaches
+     1. Group together strings with the same character frequency 
+
+    Data Structures
+     - Hash map
+     - Array
+    Algorithms
+
+    Time Complexity
+     - O(n•k) -> O(n•k) -> n = number of words, k = average length of each word
+    Space Complexity
+     - O(n•k)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Solution 1
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for (const auto& s : strs) {
+            string t = s;
+            sort(t.begin(), t.end());
+            mp[t].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+
+        for (const auto& [key, anagrams] : mp) {
+            ans.push_back(anagrams);
+        }
+
+        return ans;
+    }
+};
+
+// Solution 1.1
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for (const auto& s : strs) {
+            vector<int> freq(26);
+            for (int i = 0; i < s.length(); i++) {
+                freq[s[i] - 'a']++;
+            }
+            string freqS;
+            for (int i = 0; i < 26; i++) {
+                freqS += '#' + to_string(i) + to_string(freq[i]);
+            }
+            mp[freqS].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+
+        for (const auto& [key, anagrams] : mp) {
+            ans.push_back(anagrams);
+        }
+
+        return ans;
+    }
+};
